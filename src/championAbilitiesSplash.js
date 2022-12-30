bc9b5b0006a632cf30e0a5ff68e1fe616ab1a083
Ben Stroup <bstroup@iastate.edu>
2022-12-30 17:18:17 -0600

    change some css and add icon

--- src/championAbilitiesSplash.js
+++ src/championAbilitiesSplash.js
@@ -5,7 +5,7 @@ const championAbilitiesSplash = (props) => {
     const abilityImages = props.abilities.map((spell, index) => {
         if (spell === props.currentAbility) {
             return (
-                <div className="col-2" key={spell.id}>
+                <div className={style.champSplash} key={spell.id}>
                     <img onClick={() => props.onAbilityClick(index)}
                         src={`http://ddragon.leagueoflegends.com/cdn/12.23.1/img/spell/${spell.image.full}`}
                         alt={spell.name + " pic"} 
@@ -18,7 +18,7 @@ const championAbilitiesSplash = (props) => {
 
 
 
-            <div className="col-2"
+            <div className={style.champSplash}
                 key={spell.id}><img onClick={() => props.onAbilityClick(index)}
                     src={`http://ddragon.leagueoflegends.com/cdn/12.23.1/img/spell/${spell.image.full}`}
                     alt={spell.name + " pic"} />
