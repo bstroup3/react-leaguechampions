186ef4d81350013c6afb3f6e23123b91340048ea
Ben Stroup <bstroup@iastate.edu>
2023-03-15 21:45:30 -0500

    fix passive images by passing in version

--- src/champion/passive/championPassive.js
+++ src/champion/passive/championPassive.js
@@ -3,13 +3,15 @@ import style from '../../mystyle.module.css'
 
 
 
-const championPassive = (championData) => {
+const championPassive = (championData, version) => {
     const passive = championData.championData.passive
     const desc = passive.description
+    console.log(`http://ddragon.leagueoflegends.com/cdn/${championData.version}/img/passive/${passive.image.full}`)
+    console.log(championData.version)
     return(
         <div>
             <h3>{passive.name}</h3>
-            <img className={style.passiveImage} src={`http://ddragon.leagueoflegends.com/cdn/${props.version}}/img/passive/${passive.image.full}`} alt={passive.name + 'pic'}/><br/>
+            <img className={style.passiveImage} src={`http://ddragon.leagueoflegends.com/cdn/${championData.version}/img/passive/${passive.image.full}`} alt={passive.name + 'pic'}/><br/>
             <div dangerouslySetInnerHTML={{__html: desc}}/>
         </div>
     )
