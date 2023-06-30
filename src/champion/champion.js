e6f3ae577f1c45cd20439b100895e6f00b88b250
Ben Stroup <bstroup@iastate.edu>
2023-06-30 16:02:25 -0500

    "techincally" working champ screens

--- src/champion/champion.js
+++ src/champion/champion.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import style from '../mystyle.module.css'
+import {Link} from 'react-router-dom'
+
 export default function champion({champInfo, onChampionClick, version}) {
   return (
     <div className={style.champ}>
-        <div className={style.champListBox} onClick={() => onChampionClick(champInfo)}>
+        <Link to={`/champions/${champInfo.name}`} className={style.champListBox} onClick={() => onChampionClick(champInfo)}>
             <img className={style.championSquare} src={`http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champInfo.image.full}`}
             alt={champInfo.name + " picture"} />
             <p className={style.champName}>{champInfo.name.toUpperCase()}</p>
-        </div>
+        </Link>
     </div>
   );
 }
