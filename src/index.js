c14287bd3f7abb66b26481b26af0036381b91e90
Ben Stroup <bstroup@iastate.edu>
2023-07-14 21:21:43 -0500

    add permission policy

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <>
+    <meta http-equiv="Permissions-Policy" content="interest-cohort=()"></meta>
     <App />
+    </>
 );
 
