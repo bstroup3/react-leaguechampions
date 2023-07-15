52ece0d1126fc01831230a81e76a9fa4946f5571
Ben Stroup <bstroup@iastate.edu>
2023-07-14 22:04:11 -0500

    add additional file (not needed) fix deploy maybe?

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
-    <meta http-equiv="Permissions-Policy" content="interest-cohort=()"></meta>
     <App />
     </>
 );
