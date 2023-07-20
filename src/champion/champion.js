50e856322a89248ab6d74f61bd1cbb05b5ecead3
Ben Stroup <bstroup@iastate.edu>
2023-07-19 21:08:21 -0500

    add stupid temp to more places

--- src/champion/champion.js
+++ src/champion/champion.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 export default function champion({champInfo, onChampionClick, version}) {
   return (
     <div className={style.champ}>
-        <Link to={`/champions/${champInfo.name}`} className={style.champListBox} onClick={() => onChampionClick(champInfo)}>
+        <Link to={`react-leaguechampions/champions/${champInfo.name}`} className={style.champListBox} onClick={() => onChampionClick(champInfo)}>
             <img className={style.championSquare} src={`http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champInfo.image.full}`}
             alt={champInfo.name + " picture"} />
             <p className={style.champName}>{champInfo.name.toUpperCase()}</p>
