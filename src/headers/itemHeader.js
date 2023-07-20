15165776ce49ab741041e7c9a0fa4d885866d313
Ben Stroup <bstroup@iastate.edu>
2023-07-19 21:14:33 -0500

    fix back buttons

--- src/headers/itemHeader.js
+++ src/headers/itemHeader.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom"
 const header = ({currentView, onCardClickBack, onSearchChange, }) => {
     return(
       <div className={style.stickyHeader}>
-        <Link className={style.homeButton} to="/">Home</Link>
+        <Link className={style.homeButton} to="/react-leaguechampions/">Home</Link>
         <div className={style.centerBlock}>
           <h1 className={style.header}>League of Legends Item Viewer </h1>
           <input type="search" placeholder="Search Items" className={style.SearchBar} onChange={onSearchChange}/>
