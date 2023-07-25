f155e7e9732aea9e99c9b73eadf3888e356345ea
Ben Stroup <bstroup@iastate.edu>
2023-07-25 16:26:30 -0500

    Add Remake into game outcomes

--- src/profile/match.js
+++ src/profile/match.js
@@ -57,17 +57,19 @@ export default function Match({matchId, api_key, profileId}){
         }
     ]
     if(match != undefined){
+        console.log(match)
         const playerTeam = match.info.participants.filter((participant) => (participant.puuid == profileId))[0].teamId
-        const gameOutcome = (match.info.teams[0].teamId == playerTeam && match.info.teams[0].win == true) || (match.info.teams[1].teamId == playerTeam && match.info.teams[1].win == true) ? "Win" : "Loss" //playerTeam == 100 ? "Win" : "Loss"
-        const gameStartDate = new Date(match.info.gameCreation).toISOString().slice(0,10)
-        const gameStartTime = new Date(match.info.gameCreation).toISOString().slice(11,16)
+        let gameOutcome = (match.info.teams[0].teamId == playerTeam && match.info.teams[0].win == true) || (match.info.teams[1].teamId == playerTeam && match.info.teams[1].win == true) ? "Win" : "Loss" 
+        gameOutcome = (match.info.gameDuration < 600) ? "Remake" : gameOutcome
+        const gameDate = new Date(match.info.gameEndTimestamp).toISOString().slice(0,10)
+        const gameTime = new Date(match.info.gameEndTimestamp).toISOString().slice(11,16)
         const gameDuration = secondsToTime(match.info.gameDuration)
         const gameMode = queues.filter((queue) => (queue.queueId == match.info.queueId))[0].name
         return(
             <div className={style.matchContainer}>
                 <h2>{gameMode}</h2>
                 <h1>{gameOutcome}</h1>
-                <h3>{gameStartDate} {gameStartTime}</h3>
+                <h3>{gameDate} {gameTime}</h3>
                 <h3>{gameDuration}</h3>
             </div>
         )
