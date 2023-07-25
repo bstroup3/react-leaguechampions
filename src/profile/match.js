47a8207a7f192ff61a7f2883c956853159eab4c0
Ben Stroup <bstroup@iastate.edu>
2023-07-25 12:49:40 -0500

    Added Match History

---
+++ src/profile/match.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import style from './profile.module.css'
+import Axios from 'axios'
+import {Link} from 'react-router-dom'
+
+export default function Match({matchId, api_key, profileId}){
+    const [match, setMatch] = useState()
+    useEffect(() => {
+        Axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=${api_key}`)
+        .then((response) => {
+            setMatch(response.data)
+        })
+    },[])
+    function secondsToTime(secs){
+        var h = Math.floor(secs / (60 * 60));
+      
+        var divisor_for_minutes = secs % (60 * 60);
+        var m = Math.floor(divisor_for_minutes / 60);
+      
+        var divisor_for_seconds = divisor_for_minutes % 60;
+        var s = Math.ceil(divisor_for_seconds);
+      
+        return `${h?`${h}:`:""}${m?`${m}:${s}`:`${s}s`}`
+    }
+    const queues = [
+        {
+            queueId: 1700,
+            name: "Arena"
+        },
+        {
+            queueId: 420,
+            name: "Ranked Solo"
+        },
+        {
+            queueId: 430,
+            name: "Blind"
+        },
+        {
+            queueId: 440,
+            name: "Ranked Flex"
+        },
+        {
+            queueId: 450,
+            name: "ARAM"
+        },
+        {
+            queueId: 700,
+            name: "Clash"
+        },
+        {
+            queueId: 720,
+            name: "ARAM Clash"
+        },
+        {
+            queueId: 400,
+            name: "Draft"
+        }
+    ]
+    if(match != undefined){
+        const playerTeam = match.info.participants.filter((participant) => (participant.puuid == profileId))[0].teamId
+        const gameOutcome = (match.info.teams[0].teamId == playerTeam && match.info.teams[0].win == true) || (match.info.teams[1].teamId == playerTeam && match.info.teams[1].win == true) ? "Win" : "Loss" //playerTeam == 100 ? "Win" : "Loss"
+        const gameStartDate = new Date(match.info.gameCreation).toISOString().slice(0,10)
+        const gameStartTime = new Date(match.info.gameCreation).toISOString().slice(11,16)
+        const gameDuration = secondsToTime(match.info.gameDuration)
+        const gameMode = queues.filter((queue) => (queue.queueId == match.info.queueId))[0].name
+        return(
+            <div className={style.matchContainer}>
+                <h2>{gameMode}</h2>
+                <h1>{gameOutcome}</h1>
+                <h3>{gameStartDate} {gameStartTime}</h3>
+                <h3>{gameDuration}</h3>
+            </div>
+        )
+    }
+    else{
+        return(
+            <h2 className={style.header}>Loading...</h2>
+        )
+    }
+}
--- src/profile/profileViewer.js
+++ src/profile/profileViewer.js
@@ -3,6 +3,7 @@ import style from "./profile.module.css"
 import Header from "../headers/profileHeader"
 import Axios from "axios";
 import NoPage from "../nopage/noPageFound"
+import Match from "./match.js"
 import { redirect } from "react-router-dom";
 
 export default function ProfileViewer({championsData, version}){
@@ -12,6 +13,8 @@ export default function ProfileViewer({championsData, version}){
     const api_key = process.env.REACT_APP_API_KEY
     const [profile, setProfile] = useState("")
     const [championMastery, setChampionMastery] = useState()
+    const [matchHistory, setMatchHistory] = useState()
+    const [matches, setMatches] = useState([])
     useEffect(() => {
         Axios.get(`https://${server}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${api_key}`)
         .then((response) => {
@@ -20,29 +23,31 @@ export default function ProfileViewer({championsData, version}){
             .then((response) => {
                 setChampionMastery(response.data)
             })
+            Axios.get(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${response.data.puuid}/ids?start=0&count=15&api_key=${api_key}`)
+            .then((response) => {
+                setMatchHistory(response.data)
+            })
         })
         .catch((e) =>{
             setResponseCode(e.name)
         })
     },[])
+    console.log(matches)
     if(responseCode == "AxiosError"){
         return(<NoPage/>)
     }
-    else if(profile != undefined && championMastery != undefined){
-        const id = profile.id
-        const accountId = profile.accountId
-        const puuid = profile.puuid
+    else if(profile != undefined && championMastery != undefined && matchHistory != undefined){
         const preFilterChampionMastery = championMastery.slice(0,3)
         const newUsername = profile.name
-        console.log(championMastery.slice(0,3))
         const filteredChampionMasteryName = preFilterChampionMastery.map((champion1) => {
-            console.log(champion1)
             return <h3 className={style.masteryComponents}>{championsData.filter((champion2) => (champion1.championId == champion2.key))[0].name}<br/>Mastery Level {champion1.championLevel}<br/>{champion1.championPoints}</h3>
-         })
-         const filteredChampionMasteryPictureName = preFilterChampionMastery.map((champion1) => {
+        })
+        const filteredChampionMasteryPictureName = preFilterChampionMastery.map((champion1) => {
             return <img className={style.masteryPicture} src={`http://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${championsData.filter((champion2) => (champion1.championId == champion2.key))[0].image.full}`} />
-         })
-         console.log(filteredChampionMasteryPictureName)
+        })
+        const matches = matchHistory.map((matchId) => {
+            return <Match matchId={matchId} api_key={api_key} profileId={profile.puuid}/> 
+        })
         return(
             <div>
                 <Header username={newUsername}/>
@@ -54,6 +59,9 @@ export default function ProfileViewer({championsData, version}){
                         {filteredChampionMasteryName}
                     </div>
                 </div>
+                <h2 className={style.header}>Match History</h2>
+                <hr className={style.lineRow}/>
+                {matches}
             </div>
         )
     }
