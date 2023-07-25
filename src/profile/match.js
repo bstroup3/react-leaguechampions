6062445b7ec7bc58e99f420b94744883339bee56
Ben Stroup <bstroup@iastate.edu>
2023-07-25 18:17:22 -0500

    fix color scheme and fix time on profile lookup

--- src/profile/match.js
+++ src/profile/match.js
@@ -62,16 +62,17 @@ export default function Match({matchId, api_key, profileId}){
         let gameOutcome = (match.info.teams[0].teamId == playerTeam && match.info.teams[0].win == true) || (match.info.teams[1].teamId == playerTeam && match.info.teams[1].win == true) ? "Win" : "Loss" 
         gameOutcome = (match.info.gameDuration < 600) ? "Remake" : gameOutcome
         const offset = new Date(match.info.gameEndTimestamp).getTimezoneOffset()
-        console.log(offset)
-        const gameDate = new Date(match.info.gameEndTimestamp).toISOString().slice(0,10)
-        const gameTime = new Date(match.info.gameEndTimestamp).toISOString().slice(11,16)
+        const date = new Date(match.info.gameEndTimestamp).toLocaleString()
+        const gameDate = date.slice(0,10)
+        const gameTime = date.slice(11,15)
+        const gameTimeSequence = date.slice(18,29)
         const gameDuration = secondsToTime(match.info.gameDuration)
         const gameMode = queues.filter((queue) => (queue.queueId == match.info.queueId))[0].name
         return(
             <div className={style.matchContainer}>
                 <h2>{gameMode}</h2>
                 <h1>{gameOutcome}</h1>
-                <h3>{gameDate} {gameTime}</h3>
+                <h3>{gameDate} {gameTime}{gameTimeSequence}</h3>
                 <h3>{gameDuration}</h3>
             </div>
         )
